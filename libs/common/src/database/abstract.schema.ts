03203b609de4f0a765b92ded553eb5a501deea37
Jonathan <jonathan_colamaio@solutionbox.com.ar>
2023-01-23 12:21:57 -0300

    orders schena

--- libs/common/src/database/abstract.schema.ts
+++ libs/common/src/database/abstract.schema.ts
@@ -1,8 +1,8 @@
 import { Prop, Schema } from '@nestjs/mongoose';
-import {Schematypes, Types } from 'mongoose'
+import {SchemaTypes, Types } from 'mongoose'
 
 @Schema()
 export class AbstractDocument {
-    @Prop({ type: Schematypes.ObjectId })
+    @Prop({ type: SchemaTypes.ObjectId })
     _id: Types.ObjectId
 }
