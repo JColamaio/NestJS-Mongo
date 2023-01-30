2e22120ce8677de2cf53d1402a36528be0242bf1
Jonathan <jonathan_colamaio@solutionbox.com.ar>
2023-01-30 14:48:11 -0300

    billing rmq service, module, orders

--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,18 +1,35 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
-import { CreateOrderRequest } from '../dto/create-order.request';
-import { BILLING_SERVICE } from './constants/services';
-import { OrdersRespository } from './orders.repository';
+import { Inject, Injectable } from "@nestjs/common";
+import { ClientProxy } from "@nestjs/microservices";
+import { lastValueFrom } from "rxjs";
+import { CreateOrderRequest } from "../dto/create-order.request";
+import { BILLING_SERVICE } from "./constants/services";
+import { OrdersRespository } from "./orders.repository";
 
 @Injectable()
 export class OrdersService {
-  constructor(private readonly orderRespository: OrdersRespository, @Inject(BILLING_SERVICE) private billingClient: ClientProxy) {}
+  constructor(
+    private readonly orderRespository: OrdersRespository,
+    @Inject(BILLING_SERVICE) private billingClient: ClientProxy,
+  ) {}
 
   async createOrder(request: CreateOrderRequest) {
-    return this.orderRespository.create(request)
+    const session = await this.orderRespository.startTransaction()
+    try {
+      const order = await this.orderRespository.create(request, {session})
+      await lastValueFrom(
+        this.billingClient.emit('order_created', {
+          request,
+        })
+      );
+      await session.commitTransaction();
+      return order;
+    } catch (err) {
+      await session.abortTransaction()
+      throw err
+    }
   }
 
   async getOrders() {
-    return this.orderRespository.find({})
+    return this.orderRespository.find({});
   }
 }
